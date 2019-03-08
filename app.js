f80a590b1e46ef5c0dec12e79a55076d75ae8694
Clément <clement@meetluseed.com>
2019-03-08 10:17:02 +0100

    creation des routes

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const userRouter = require('./routes/user');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -15,6 +14,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/user', userRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
